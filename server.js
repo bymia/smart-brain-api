b186f20e4aeb50ebf97eb8f67d39cc2bd818a722
mia <wanderfly336@gmail.com>
2023-06-18 13:24:06 +0800

    Configured API

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    host : process.env.DATABASE_HOST,
+    port : 5432,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send('HOLLO WORLD')});
+app.post('/signin', (req, res) =>{signin.handleSignin(req, res, db, bcrypt)});
+app.post('/register', (req, res) =>{register.handleRegister(req, res, db, bcrypt)});
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+app.post('/image', (req, res) => {image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+const port = process.env.PORT || 3000;
+app.listen(port || 3000, () => {
+  console.log(`app is running on ${port}`);
+})
